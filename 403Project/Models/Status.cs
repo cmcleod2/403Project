4e3dc094ed7a5ece0af4a5ddaeaaa08bda5ef03f
Bonnie McDougal <mcdouga3@byu.edu>
2019-11-19 18:40:34 -0700

    yuuup

---
+++ 403Project/Models/Status.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _403Project.Models
+{
+    //[Table("Statuses")]
+    public class Status
+    {
+        //[Key]
+        //[HiddenInput(DisplayValue = false)]
+        public int StatusID { get; set; }
+
+        //[Required(ErrorMessage = "Please enter the project type")]
+        //[StringLength(25, MinimumLength = 5)]
+        //[DisplayName("Project Type")]
+        public string StatusDesc { get; set; }
+    }
+}

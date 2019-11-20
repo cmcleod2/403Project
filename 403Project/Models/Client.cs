41f95b8615face0aa13a7fb565cd8be881a6b268
cmcleod2 <56420438+cmcleod2@users.noreply.github.com>
2019-11-19 18:25:35 -0700

    Updated Client Model

---
+++ 403Project/Models/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _403Project.Models
+{
+    [Table("Clients")]
+    public class Client
+    {
+        [Key]
+        [HiddenInput(DisplayValue = false)]
+        public int ClientID { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Client First Name")]
+        [DisplayName("Client First Name")]
+        public string ClFirstName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Client Last Name")]
+        [DisplayName("Client Last Name")]
+        public string ClLastName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Client Phone Number")]
+        [DisplayName("Client Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [StringLength(10)]
+        public string ClPhone { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Client Email")]
+        [DisplayName("Client Email")]
+        [DataType(DataType.EmailAddress)]
+        public string ClEmail { get; set; }
+    }
+}
